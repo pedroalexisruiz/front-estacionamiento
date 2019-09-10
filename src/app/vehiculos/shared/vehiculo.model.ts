025649d5e1529a2ebe1842c6f503032601503401
Pedro Ruiz <alexisruiz.selgom@gmail.com>
2019-09-09 19:31:08 -0500

    Auto-generated commit

--- src/app/vehiculos/shared/vehiculo.model.ts
+++
@@ -1,9 +0,0 @@
-export interface IVehiculo {
-  id: number;
-  placa: string;
-  tipoDeVehiculo: string;
-  cilindraje?: number;
-  totalAPagar?: number;
-  horaDeEntrada?: Date
-  horaDeSalida?: Date
-}
